191dcb2f0f7d5217de650f35cae5c54e5707664f
vandanamajji <145001676+vandanamajji@users.noreply.github.com>
2024-04-19 07:31:29 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -6,93 +6,115 @@ class Scratch3YourExtension {
 
     constructor (runtime) {
         import('syllable')
-          .then((syllableModule) => {
-            this.syllable = syllableModule.syllable;
-          });
-      }
+            .then((syllableModule) => {
+                this.syllable = syllableModule.syllable;
+            });
+        // put any setup for your extension here
+    }
 
     /**
      * Returns the metadata about your extension.
      */
     getInfo () {
         return {
-          // unique ID for your extension
-          id: 'yourScratchExtension',
-      
-          // name displayed in the Scratch UI
-          name: 'Demo',
-      
-          // colours to use for your extension blocks
-          color1: '#000099',
-          color2: '#660066',
-      
-          // your Scratch blocks
-          blocks: [
-            {
-              // function where your code logic lives
-              opcode: 'myFirstBlock',
-      
-              // type of block
-              blockType: BlockType.REPORTER,
-      
-              // label to display on the block
-              text: 'Title for ISBN book [BOOK_NUMBER]',
-      
-              // arguments used in the block
-              arguments: {
-                BOOK_NUMBER: {
-                  defaultValue: 1718500564,
-      
-                  // type/shape of the parameter
-                  type: ArgumentType.NUMBER
+            // unique ID for your extension
+            id: 'yourScratchExtension',
+
+            // name that will be displayed in the Scratch UI
+            name: 'Demo',
+
+            // colours to use for your extension blocks
+            color1: '#000099',
+            color2: '#660066',
+
+            // icons to display
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            // your Scratch blocks
+            blocks: [
+                {
+                    // name of the function where your block code lives
+                    opcode: 'bookBlock',
+
+                    // type of block - choose from:
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Title for ISBN book [BOOK_NUMBER]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // arguments used in the block
+                    arguments: {
+                        BOOK_NUMBER: {
+                            defaultValue: 1718500564,
+
+                            // type/shape of the parameter - choose from:
+                            type: ArgumentType.NUMBER
+                        }
+                       
+                        
+                    }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'syllBlock',
+
+                     // type of block - choose from:
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Syllables in [MY_TEXT]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // arguments used in the block
+                     arguments: {
+                        MY_TEXT: {
+                            defaultValue: 'Stem World',
+
+                            // type/shape of the parameter - choose from:
+                            type: ArgumentType.STRING
+                        }
+                    }
                 }
-              }
-            },
-            {
-              // function where your code logic lives
-              opcode: 'mySecondBlock',
-      
-              // type of block
-              blockType: BlockType.REPORTER,
-      
-              // label to display on the block
-              text: 'Syllables in [MY_TEXT]',
-      
-              // arguments used in the block
-              arguments: {
-                MY_TEXT: {
-                  defaultValue: 'Stem World',
-      
-                  // type/shape of the parameter
-                  type: ArgumentType.STRING
-                }
-              }
-            }
-          ]
+            
+        
+            ]
         };
-      }
+    }
+
     /**
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({ BOOK_NUMBER }) {
-        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
+    bookBlock({ BOOK_NUMBER }) {
+        return fetch('https://openlibrary.org/isbn/${BOOK_NUMBER}.json', {
+          method: 'GET'
+        })
           .then((response) => {
             if (response.ok) {
               return response.json();
-            }
-            else {
-              return { title: 'Unknown' };
+            } else {
+              throw new Error('Book not found');
             }
           })
           .then((bookinfo) => {
             return bookinfo.title;
+          })
+          .catch((error) => {
+            return 'No book exists';
           });
-      }
-    mySecondBlock ({ MY_TEXT }) {
+        }
+        
+
+    syllBlock ({ MY_TEXT }) {
         console.log('syllable')
-        return this.syllable(MY_TEXT);
-      }
+        return this.syllable(MY_TEXT)
+    }
 }
 
 module.exports = Scratch3YourExtension;
